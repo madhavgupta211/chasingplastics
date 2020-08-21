dad4d51400c95b8cff3ad9351cd7a98a2365ca3c
Dwij <dwij.mehta@gmail.com>
2020-08-22 02:33:24 +0530

    backend logic for contact us added

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ let Blog = require("./blog.model")
 let Podcast = require("./podcast.model")
 const blogRoutes = express.Router()
 const podcastRoutes = express.Router()
+nodeMailer = require("nodemailer")
+
 app.use(cors())
 app.use(bodyParser.json())
 require("dotenv").config()
@@ -109,6 +111,35 @@ podcastRoutes.route("/add").post(function (req, res) {
     .catch((err) => {
       res.status(400).send("adding new podcast failed")
     })
+})
+
+app.post("/contact-us", function (req, res) {
+  const id = process.env.ID
+  const pass = process.env.PASS
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      // should be replaced with real sender's account
+      user: id,
+      pass: pass,
+    },
+  })
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: "dwij.mehta@gmail.com",
+    subject: req.body.subject,
+    body: req.body.message,
+  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log("Message %s sent: %s", info.messageId, info.response)
+  })
+  res.status(200).json({ email: "emailed successfully" })
+  res.end()
 })
 
 app.use("/blogs", blogRoutes)
