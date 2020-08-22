b3ab102cced0987a4a6205bd5c5a9d409b95a75f
Dwij <dwij.mehta@gmail.com>
2020-08-22 18:44:36 +0530

    Updated to work with github pages hosting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Main from './Components/Main/mainComponent';
-
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { HashRouter as Router } from "react-router-dom"
+import Main from "./Components/Main/mainComponent"
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className = "App">
+    <Router>
+      <div className="App">
         <Main />
       </div>
-    </BrowserRouter>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
