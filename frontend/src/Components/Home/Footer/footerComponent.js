1ea195c0b2b5814fcd544c4f0390d461d5961332
Dwij <dwij.mehta@gmail.com>
2020-08-22 16:32:01 +0530

    Started on footer component

---
+++ frontend/src/Components/Home/Footer/footerComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import "./footer.css"
+
+class Footer extends Component {
+  render() {
+    return <div className="footer-bg"></div>
+  }
+}
+
+export default Footer
